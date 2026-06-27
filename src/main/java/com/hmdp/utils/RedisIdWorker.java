ff9c9b006767d66837d2fd4bc15d0a4ea36e20fb
zsy <1179611284@qq.com>
2026-06-27 10:22:01 +0800

    Redis-全局唯一ID生成器

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,45 @@
+package com.hmdp.utils;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 依赖Redis，实现全局ID生成器
+ */
+@Component
+public class RedisIdWorker {
+
+    /**
+     * 开始时间戳：2022-01-01 00:00:00
+     */
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+
+    /**
+     * 序列号的位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    public long nextId(String keyPrefix){
+        //生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        //序列号
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        Long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        //拼接
+        return timestamp << COUNT_BITS | count;
+
+    }
+}
